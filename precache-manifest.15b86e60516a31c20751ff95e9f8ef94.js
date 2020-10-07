bc2031687b034e910456c9c61accbaa90675cfbd
Sales <guilherme.sales@sap.com>
2020-10-07 17:26:41 -0300

    build

---
+++ precache-manifest.15b86e60516a31c20751ff95e9f8ef94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85f72be8454d498e827d4c8e835b356",
+    "url": "/joker/index.html"
+  },
+  {
+    "revision": "6e671df3259d29d75dfc",
+    "url": "/joker/static/css/main.3dd42017.chunk.css"
+  },
+  {
+    "revision": "44a6af7726b20cfb016a",
+    "url": "/joker/static/js/2.14c32059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joker/static/js/2.14c32059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e671df3259d29d75dfc",
+    "url": "/joker/static/js/main.d190664a.chunk.js"
+  },
+  {
+    "revision": "3ff68e3e42567cc027ad",
+    "url": "/joker/static/js/runtime-main.8638c99f.js"
+  }
+]);
